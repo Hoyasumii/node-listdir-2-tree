d6a031e9899de054c5db768a92da3e83997d2774
Alan Reis <alanreisanjo@gmail.com>
2024-10-30 19:01:07 -0300

    Ajustes

--- index.ts
+++ index.ts
@@ -1,11 +1,50 @@
-import { readdir, stat } from 'node:fs/promises';
+import { readdir, readFile } from "node:fs/promises";
+import { parse } from "acorn";
 
 async function list(path: string = "./") {
-  const dir = await readdir(path, { withFileTypes: true })
+  const dir = await readdir(path, { withFileTypes: true });
+  const selectedFile = dir.filter((file) => file.name.endsWith(".ts"))[0];
 
-  console.log(dir[0].isFile());
+  const myFile = await readFile(`${path}/${selectedFile.name}`, {
+    encoding: "utf8",
+  });
+
+  const tsDoc = parse(myFile, { ecmaVersion: "latest", sourceType: "module" });
+
+  console.log(tsDoc.);
+
+  // console.log(dir[0].isFile());
   // console.log(await stat(`${path}/${dir[0].isDirectory()}`))
 }
 
-await list();
+await list("./testing");
 
+
+
+async function listMethods(filePath: string) {
+  const fileContent = await readFile(filePath, 'utf-8');
+  const ast = parse(fileContent, { ecmaVersion: 'latest', sourceType: 'module' });
+
+  const methods: string[] = [];
+
+  function extractMethods(node: any) {
+    if (node.type === 'FunctionDeclaration' || node.type === 'FunctionExpression' || node.type === 'ArrowFunctionExpression') {
+      if (node.id && node.id.name) {
+        methods.push(node.id.name);
+      }
+    }
+    for (const key in node) {
+      if (node[key] && typeof node[key] === 'object') {
+        extractMethods(node[key]);
+      }
+    }
+  }
+
+  extractMethods(ast);
+  return methods;
+}
+
+(async () => {
+  const methods = await listMethods('./path/to/your/file.js');
+  console.log('Methods:', methods);
+})();
---
+++ testing/route.ts
@@ -0,0 +1,5 @@
+export function GET() {}
+export function POST() {}
+export function PUT() {}
+export function PATCH() {}
+export function DELETE() {}
